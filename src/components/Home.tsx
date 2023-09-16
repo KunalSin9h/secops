af1e5062fc11adbb0716712bbed6d3ab4ad596c1
Kunal Singh <kunalsin9h@gmail.com>
2023-09-16 23:44:16 +0530

    initiated home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,6 @@
 import { rspc } from "@/context/rspc";
 import { ScrollArea } from "./ui/scroll-area";
+import { StoppedIcon } from "@/lib/icons";
 
 export default function Home() {
   return (
@@ -17,7 +18,39 @@ export default function Home() {
         </div>
         <ScrollArea className="border rounded shadow p-2 h-full"></ScrollArea>
       </div>
-      <ScrollArea className="w-1/2 border rounded shadow p-2"></ScrollArea>
+      <ScrollArea className="w-1/2 border rounded shadow p-2">
+        <div className="">
+          <p className="font-semibold text-2xl">Services</p>
+          <div className="flex justify-around mt-4">
+            <div>
+              <div className="flex items-center gap-2">
+                <AnimatePing />
+                <span className="text-xl">Running</span>
+              </div>
+              <ul className="mt-4">
+                {new Array(10).fill(0).map((_, idx) => (
+                  <li key={idx} className="p-1 w-36 mb-2">
+                    Hello
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <div className="flex items-center gap-2">
+                <StoppedIcon />
+                <span className="text-xl">Stopped</span>
+              </div>
+              <ul className="mt-4">
+                {new Array(5).fill(0).map((_, idx) => (
+                  <li key={idx} className="p-1 w-36 mb-2">
+                    Hello
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </ScrollArea>
     </div>
   );
 }
@@ -48,5 +81,15 @@ function Ubuntu() {
       <span className="text-purple-800 font-semibold">{release}</span>
       <span className="font-bold text-2xl">{distro}</span>
     </div>
+  );
+}
+
+// https://tailwindcss.com/docs/animation#ping
+function AnimatePing() {
+  return (
+    <span className="relative flex h-3 w-3">
+      <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+      <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+    </span>
   );
 }
