6aecb0508a642324208f934c36619f94e87bc0d0
Kunal Singh <kunalsin9h@gmail.com>
2023-10-04 13:44:17 +0530

    imporve commit

--- src/components/Header/Commit.tsx
+++ src/components/Header/Commit.tsx
@@ -9,6 +9,16 @@ import {
 } from "@/components/ui/sheet";
 import { CommitIcon } from "@/lib/icons";
 import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import {
   type StateMeta,
   getCommitStatus,
   commitSettings,
@@ -23,7 +33,7 @@ export default function Commit() {
           <span>Commit</span>
         </div>
       </SheetTrigger>
-      <SheetContent className=" w-2/3 xl:w-1/3">
+      <SheetContent className="w-2/3 xl:w-1/3 overflow-y-scroll">
         <SheetHeader>
           <SheetTitle>Commit Settings!</SheetTitle>
           <SheetDescription>
@@ -41,6 +51,8 @@ export default function Commit() {
 
 function Commits() {
   const [commitStatus, setCommitStatus] = useState<StateMeta[]>();
+  const [commitMessage, setCommitMessage] = useState("");
+  const [commitMessageDialog, setCommitMessageDialog] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -53,33 +65,53 @@ function Commits() {
 
   return (
     <div>
-      <div className="flex flex-col gap-4 px-4 xl:px-16">
+      <div className="flex flex-col gap-4 px-4 xl:px-8 my-4 xl:my-8">
         <span className="text-md">{commitStatus[0].message}</span>
         <div className="flex gap-4 items-center">
+        <Dialog open={commitMessageDialog}>
+          <DialogTrigger className={`${commitStatus[0].commit ? "pointer-events-none cursor-not-allowed opacity-60" : "" }`}>
           <div
-            className={`${
-              commitStatus[0].commit ? "bg-green-100" : "bg-green-300"
-            }  text-black ${
-              commitStatus[0].commit
-                ? "pointer-events-none cursor-not-allowed opacity-60"
-                : "hover:bg-green-400/50"
-            } px-3 py-2 rounded-md `}
-            onClick={async (e) => {
-              e.preventDefault();
-              await commitSettings("new commit it is");
-              const status = await getCommitStatus();
-              setCommitStatus(status);
-            }}
-          >
-            <span className="text-xs uppercase font-bold">
-              {commitStatus[0].commit ? "All Good" : "Commit"}
-            </span>
+              className={`${
+                  commitStatus[0].commit ? "bg-green-100" : "bg-green-300"
+                  }  text-black 
+                  "hover:bg-green-400/50"
+                   px-3 py-2 rounded-md `}
+                onClick={(e) => {
+                    e.preventDefault();
+                    setCommitMessageDialog(true);
+                }}
+              >
+              <span className="text-xs uppercase font-bold">
+                  {commitStatus[0].commit ? "All Good" : "Commit"}
+              </span>
           </div>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Commit current settings</DialogTitle>
+            </DialogHeader>
+            <div>
+                <Input placeholder="Commit message..." onChange={(e) => {
+                    e.preventDefault();
+                    setCommitMessage(e.target.value);
+                }} />                
+                <Button className="mt-4" 
+                  onClick={async (e) => {
+                      e.preventDefault();
+                      await commitSettings(commitMessage);
+                      const status = await getCommitStatus();
+                      setCommitMessageDialog(false);
+                      setCommitStatus(status);
+                  }}
+                >Commit</Button>
+            </div>
+          </DialogContent>
+        </Dialog>
           <span className="opacity-60 text-xs">{commitStatus[0].time}</span>
         </div>
       </div>
 
-      <div className="py-4 xl:py-8">
+      <div>
         {commitStatus.slice(1).map((item, index) => {
           return <CommitBox key={index} {...item} />;
         })}
@@ -98,7 +130,7 @@ function CommitBox({
   time: string;
 }) {
   return (
-    <div className="flex flex-col gap-4 px-4 xl:px-16">
+    <div className="flex flex-col gap-4 px-4 xl:px-8 my-4 xl:my-8">
       <span className="text-md">{message}</span>
       <div className="flex gap-4 items-center">
         <div
